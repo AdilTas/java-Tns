9c33c32d13ae39238e2c645d751f61ad9201e730
AdilTas <adilbadi1325@gmail.com>
2024-06-26 16:48:12 +0530

    Add files via upload

---
+++ MyFirstTest.java
@@ -0,0 +1,13 @@
+package com.tnsif.daytwentytwo.testcases;
+
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+
+	@Test
+	void display()
+	{
+		System.out.println("Hello world");
+	}
+	
+}
